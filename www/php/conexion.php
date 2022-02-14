dc2747d861d93ae817feaf2d6083f18ffa889aa8
jdvpl <juanda554242@gmail.com>
2022-02-13 19:03:40 -0500

    mostrar usuarios

--- www/php/conexion.php
+++ www/php/conexion.php
@@ -5,7 +5,7 @@ $conexion=mysqli_connect("mysql","jdvpl","jdvpl","crud_docker_php","3306");
 if(mysqli_connect_errno()){
     echo "conexion fallida".mysqli_connect_errno();
 }else{
-    echo "conexion exitosa";
+    echo "";
 }
 
 mysqli_query($conexion,"SET NAMES 'utf8'");
